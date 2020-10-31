e8e9dfe4ab4cec1936245a318e0f7092779e771f
arturo payload <anegreiross@outlook.com>
2020-10-30 22:47:44 -0500

    ok

--- machinelearning2.py
+++ machinelearning2.py
@@ -104,3 +104,41 @@ x = torch.from_numpy(data).float().requires_grad_(True)
 y = torch.from_numpy(target).float()
 
 x.shape, y.shape
+
+# como la salida va a ser una probabilidad
+# para la función sigmoide
+model =  nn.Sequential(
+    nn.Linear(2,1),
+    nn.Sigmoid()
+)
+loss_function = nn.BCELoss()
+optimizer = optim.SGD(model.parameters(), lr=0.01)
+losses = []
+iteractions = 1000
+
+for i in range(iteractions):
+  result = model(x)
+  loss = loss_function(result, y)
+  losses.append(loss.data)
+
+  optimizer.zero_grad()
+  loss.backward()
+
+  optimizer.step()
+
+plt.plot(range(iteractions), losses)
+loss
+
+blue = torch.Tensor([[-5, -6]])
+prediction = model(blue).data[0][0] > 0.5
+# si va más de .5 será rojo caso contrario será azul
+print(colors[prediction])
+
+w = list(model.parameters())
+w0 = w[0].data.numpy()
+w1 = w[1].data.numpy()
+
+plt.scatter(data[:, 0], data[:,1], c=color, s= 75, alpha=0.6)
+x_axis = np.linspace(-10,10,n)
+y_axis = -(w1[0] + x_axis*w0[0][0]) / w0[0][1]
+plt.plot(x_axis, y_axis, 'g--')
