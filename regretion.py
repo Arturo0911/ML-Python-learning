6513d9b225c0b06fdf7a6c4516441b2cf0774f38
Arturon <anegreiross@outlook.com>
2020-11-02 14:38:40 -0500

    97 % of training

--- regretion.py
+++ regretion.py
@@ -21,6 +21,21 @@ data['feedback'] = [] # aquí van los valores que no deben ser incluídos como p
 global nota1
 global nota2 
 
+
+class Machine_learning:
+    
+    def __init__(self):
+        pass
+
+    def _save_into_json_file(self):
+        pass
+
+    def _feed_back(self):
+        pass
+
+
+
+
 def first_test():
 
     
@@ -51,21 +66,29 @@ def final_test():
 
 # Método para crear un archivo .json y con este poder guardar los datos para que me sirva como parte del feedback
 
-def created_json_file(value_1, value_2):
+def created_json_file(data_to_storage):
     
 
-    data['feedback'].append({'peso_1': value_1, "peso_2": value_2})
+    #data['feedback'].append({'peso_1': value_1, "peso_2": value_2})
     with open('data.json', 'w') as file:
-        json.dump(data, file, indent=4)
+        json.dump(data_to_storage, file, indent=4)
 
     
 
+# read file again and after that storage again the data
 def feed_back():
-    pass
+
+    with open('data.json') as file:
+        reader = json.load(file)
+
+        for x in reader['feedback']:
+            data['feedback'].append({'peso_1':x['peso_1'], 'peso_2':x['peso_2']})
+feed_back()
+def store_(value_1, value_2):
+    data['feedback'].append({'peso_1': value_1, 'peso_2': value_2})
 
 
-
-
+#print(data['feedback'])
 
 
 
@@ -93,11 +116,14 @@ if __name__ == '__main__':
             
 
             #dataset.append([peso1, peso2])
-            created_json_file(peso1, peso2)
+            store_(peso1, peso2)
+            #created_json_file(peso1, peso2)
             #nota1 = random.randint(7,10)
             #nota2 = random.randint(7,10)
             counter += 1
-        
+
+
+    created_json_file(data)
     print("la calificación final: ",final_test())
     print("El peso de la nota 1: ",peso1, " el peso de la nota 2:", peso2 )
     print("Las veces que se realizó un recorrido son: ", counter)
