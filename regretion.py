4e9e58d58528201d934c8f03037a43c42804963b
Arturon <anegreiross@outlook.com>
2020-11-02 02:34:51 -0500

    good

--- regretion.py
+++ regretion.py
@@ -2,34 +2,46 @@
 
 # predecir como un profesor aplicará las calificaciones. 
 
+
 import random
 import time
 
-nota1 = 0
-nota2 = 0
+
+dataset = [] # aquí van los valores que no deben ser incluídos como parte del análisis.
 
 
 
+global nota1
+global nota2 
 
 def first_test():
 
-    global nota1
+    
     global peso1
 
-    nota1 = 6
+    nota1 = 7
     peso1 = random.uniform(0.1,0.9)
 
     return (nota1 * peso1)
 
 def second_test():
 
-    global nota2 
+    
     global peso2
 
     nota2 = 7
     peso2 = random.uniform(0.1,0.9)
 
     return (nota2 * peso2)
+
+def final_test():
+    
+    nota_final  = float((first_test() + second_test())/(peso1 + peso2))
+
+    return nota_final
+
+
+
 
 
 
@@ -39,7 +51,7 @@ if __name__ == '__main__':
 
 
     while True:
-        nota_final = float(((first_test() + second_test()))/(peso1 + peso2))
+        final_test()
         #print("la calificación final: ",nota_final)
         #print("El peso de la nota 1: ",peso1, " el peso de la nota 2:", peso2 )
         time.sleep(1)
@@ -49,16 +61,18 @@ if __name__ == '__main__':
         print(nota_final)
         break
         """
-        if (nota_final > 7.0):
-            print("la calificación final: ",nota_final)
-            print("El peso de la nota 1: ",peso1, " el peso de la nota 2:", peso2 )
+        if (final_test() > 7.0):
+            #print("la calificación final: ",nota_final)
+            #print("El peso de la nota 1: ",peso1, " el peso de la nota 2:", peso2 )
             break
-        elif(nota_final < 6.0):
+        elif(final_test() <= 7.0):
             
 
-            print("la calificación final: ",nota_final)
-            print("El peso de la nota 1: ",peso1, " el peso de la nota 2:", peso2 )
+            
+            #nota1 = random.randint(7,10)
+            #nota2 = random.randint(7,10)
             counter += 1
         
-
+    print("la calificación final: ",final_test())
+    print("El peso de la nota 1: ",peso1, " el peso de la nota 2:", peso2 )
     print("Las veces que se realizó un recorrido son: ", counter)
