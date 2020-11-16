bd709a86d56d51e6791eaea1f1ec8915a99eaef4
Arturon <anegreiross@outlook.com>
2020-11-16 10:26:31 -0500

    Meteorology added

---
+++ meteorology/Meteorology.py
@@ -0,0 +1,16 @@
+
+import torch
+import numpy as np
+import pandas as pd
+
+
+
+class Meteorology:
+
+    def __init__(self):
+
+        """
+        Download any information about weather
+        """
+
+        pass
