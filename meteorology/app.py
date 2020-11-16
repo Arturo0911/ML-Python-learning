245540c2d5ac9d9d8a05bb2118c48906cf94e64b
Arturon <anegreiross@outlook.com>
2020-11-16 17:02:25 -0500

    ok

--- meteorology/app.py
+++ meteorology/app.py
@@ -1,11 +1,26 @@
 from Meteorology import Meteorology as mt
+from pprint import pprint
+
 
 if __name__ == "__main__":
     
     meteorology = mt('Guayaquil')
 
-    print(len(meteorology.res))
+    print(meteorology.res)
 
-    for x in meteorology.res:
-        print(x, x.values)
+
+    print("\n")
+
+    
+    pprint(meteorology.res)
+    #for x, y in zip(meteorology.res, meteorology.res.values()):
+    #print(x,y)
     #meteorology.read_data()
+
+
+
+# -2.882325
+# -79.009294
+# http://186.42.174.241/InamhiPronostico/  => INAMHI
+
+# api.openweathermap.org/data/2.5/weather?lat=-2.882325&lon=-79.009294&appid=ba199bd058f45b1a54e0d25ba95d1ca9
