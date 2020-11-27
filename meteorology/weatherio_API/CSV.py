3ba0e3eba015082a7afe1417dd56e13989b1f7d8
Arturon <anegreiross@outlook.com>
2020-11-27 01:32:36 -0500

    finished storage

--- meteorology/weatherio_API/CSV.py
+++ meteorology/weatherio_API/CSV.py
@@ -9,14 +9,19 @@ from main import Create_days
 
 
 
+def create_file(directory_name,file_name):
 
-
-def create_file_with_parameters(file_name, time_start, time_end,list_values):
-
-    with open(file_name+'.csv', 'w') as csv_file:
+    with open('csv'+'/'+str(directory_name)+'/'+str(file_name)+'.csv', 'w') as csv_file:
 
         writer = csv.writer(csv_file)
         writer.writerow(["time_start", "time_end", 'Overcast_clouds', 'Broken_clouds', 'Scattered_clouds', 'Light_rain', 'Few_clouds', 'Clear_Sky'])
+    
+
+def create_file_with_parameters(directory_name,file_name, time_start, time_end,list_values):
+
+    with open('csv'+'/'+str(directory_name)+'/'+str(file_name)+'.csv', 'a') as csv_file:
+
+        writer = csv.writer(csv_file)
         writer.writerow([time_start,time_end, list_values.count('Overcast clouds'),list_values.count('Broken clouds'),list_values.count('Scattered clouds'),
         list_values.count('Light rain'),list_values.count('Few clouds'),list_values.count('Clear Sky')])
 
--- meteorology/weatherio_API/main.py
+++ meteorology/weatherio_API/main.py
@@ -9,6 +9,10 @@ import requests
 from calendar import monthrange
 from datetime import date, timedelta
 import json
+
+# import files from directory
+import CSV
+
 from API_values import API_values
 from Create_days import Create_days
 
@@ -17,7 +21,7 @@ longitude = '-80.229769'
 # time_start = '2016-10-21'
 # time_end = '2016-10-22'
 
-list_values = list()
+
 
 if __name__ == "__main__":
 
@@ -29,27 +33,38 @@ if __name__ == "__main__":
     # Initialize process
 
     new_query = API_values(latitude, longitude)
+    CSV.create_file(2018,2018)
 
     # Generate a iterator to fetch values from dates
     
-    for i in range(1, len(days.get_objects()[2019])):
+    for i in range(1, len(days.get_objects()[2018])):
 
-        time_start = days.get_objects()[2019][i-1]
-        time_end = days.get_objects()[2019][i]
+        time_start = days.get_objects()[2018][i-1]
+        time_end = days.get_objects()[2018][i]
 
         # Instantiate from API_values class
 
-        print(time_start, time_end)
+        #print(time_start, time_end)
     
         new_query.generate_process(time_start, time_end)
+        list_values = list()
+        
 
         for j in new_query.response_data['data']:
 
+            # Here store all values of description into weather key
+            # after call the function 'create_file_with_parameters()'
 
-            if j['weather']['description'] not in list_values :
-                list_values.append(j['weather']['description'])
+            list_values.append(j['weather']['description'])
 
-print(list_values)
+
+            # if j['weather']['description'] not in list_values :
+            #    list_values.append(j['weather']['description'])
+        CSV.create_file_with_parameters(2018,2018,time_start,time_end,list_values)
+
+        
+
+# print(list_values)
 
 
 
