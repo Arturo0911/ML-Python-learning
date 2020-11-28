bbc36eafd27fcefdaef778de016cb2b1797dd803
Arturon <anegreiross@outlook.com>
2020-11-28 12:44:18 -0500

    new file created, file to read csv dataframes

--- meteorology/weatherio_API/Model.py
+++ meteorology/weatherio_API/Model.py
@@ -1,6 +1,14 @@
+import torch
+import numpy as np
+import pandas as pd
 
 
 
+""" Import from csv files """
+
+
+
+# Initialize the main function from the project
 
 def main():
-    pass
+    
---
+++ meteorology/weatherio_API/read_files.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+import torch
+
+
+
+def read_csv_file():
+
+
+
+    with open as target:
+        pass
+
+    dataframe  = np.read_csv()
+
+
+    pass
+
