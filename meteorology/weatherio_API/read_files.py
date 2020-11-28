c05cff399c71f18f6861637296d080bfc994273b
Arturon <anegreiross@outlook.com>
2020-11-28 13:00:11 -0500

    added ok

--- meteorology/weatherio_API/read_files.py
+++ meteorology/weatherio_API/read_files.py
@@ -4,15 +4,23 @@ import torch
 
 
 
-def read_csv_file():
+def read_csv_file(dir_name, file_name):
 
 
+    """
+    with open('csv/values/'+str(dir_name)+'/'+str(file_name)+'.csv') as target:
+        
 
-    with open as target:
-        pass
+        dataframe  = pd.read_csv(target)
 
-    dataframe  = np.read_csv()
+        print(dataframe)
+    """
+
+    # Using the path of the files, we can open and read dataframes
+    dataframe  = pd.read_csv('csv/values/'+str(dir_name)+'/'+str(file_name)+'.csv')
+
+    print(dataframe[['time_start',  'time_end']])
 
 
-    pass
+read_csv_file(2017, 2017)
 
