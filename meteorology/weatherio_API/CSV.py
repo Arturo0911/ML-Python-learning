a404ff2df081990c90e6c10d31d8555f3b9ecc2e
Arturon <anegreiross@outlook.com>
2020-11-27 22:54:28 -0500

    newers headers created

--- meteorology/weatherio_API/CSV.py
+++ meteorology/weatherio_API/CSV.py
@@ -13,21 +13,38 @@ from main import Create_days
 # how have the clouds been
 # ['Overcast clouds', 'Broken clouds', 'Scattered clouds', 'Light rain', 'Few clouds', 'Clear Sky']
 
+""" BEHAVIOR CLOUDS """
 def create_file(directory_name,file_name):
 
-    with open('csv'+'/'+str(directory_name)+'/'+str(file_name)+'.csv', 'w') as csv_file:
+    with open('csv'+'/behavior/'+str(directory_name)+'/'+str(file_name)+'.csv', 'w') as csv_file:
 
+        # HEADERS
         writer = csv.writer(csv_file)
-        writer.writerow(["time_start", "time_end", 'Overcast_clouds', 'Broken_clouds', 'Scattered_clouds', 'Light_rain', 'Few_clouds', 'Clear_Sky'])
+        writer.writerow(["time_start", "time_end", 'Overcast_clouds', 'Broken_clouds', 
+            'Scattered_clouds', 'Light_rain', 'Few_clouds', 'Clear_Sky'])
     
 
 def create_file_with_parameters(directory_name,file_name, time_start, time_end,list_values):
 
-    with open('csv'+'/'+str(directory_name)+'/'+str(file_name)+'.csv', 'a') as csv_file:
+    with open('csv'+'/behavior/'+str(directory_name)+'/'+str(file_name)+'.csv', 'a') as csv_file:
 
         writer = csv.writer(csv_file)
-        writer.writerow([time_start,time_end, list_values.count('Overcast clouds'),list_values.count('Broken clouds'),list_values.count('Scattered clouds'),
-        list_values.count('Light rain'),list_values.count('Few clouds'),list_values.count('Clear Sky')])
+        writer.writerow([time_start,time_end, list_values.count('Overcast clouds'),list_values.count('Broken clouds'),
+            list_values.count('Scattered clouds'),list_values.count('Light rain'),list_values.count('Few clouds'),
+            list_values.count('Clear Sky')])
+
+
+""" VALUES FROM API """
+
+
+
+def create_values_csv_file(directory_name, file_name):
+
+    with open('csv'+'/values/'+str(directory_name)+'/'+str(file_name)+'.csv', 'w') as csv_file:
+        
+        # HEADERS
+        writer = csv.writer(csv_file)
+        writer.writerow(["time_start", "time_end", "clouds_5", "precip_5", "temp_5"])
 
 
 
