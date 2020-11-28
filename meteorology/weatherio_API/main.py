12050fa61994e06c75c3403b7a2efa58d80a0ee6
Arturon <anegreiross@outlook.com>
2020-11-28 00:00:59 -0500

    finished downloaded

--- meteorology/weatherio_API/main.py
+++ meteorology/weatherio_API/main.py
@@ -22,22 +22,62 @@ longitude = '-80.229769'
 # time_end = '2016-10-22'
 
 
-
 if __name__ == "__main__":
 
     # Instantiate from Create_days class
-    
+
     days = Create_days()
     days.generate_appends()
 
     # Initialize process
 
     new_query = API_values(latitude, longitude)
-    #CSV.create_file(2018,2018)
+    # CSV.create_file(2018,2018)
 
     # Generate a iterator to fetch values from dates
     # print(days.return_days_keys())
-    
+
+    CSV.create_file(2019, 2019)
+    CSV.create_values_csv_file(2019, 2019)
+
+    for i in range(1, len(days.get_objects()[2019])):
+
+        time_start = days.get_objects()[2019][i-1]
+        time_end = days.get_objects()[2019][i]
+
+        # Instantiate from API_values class
+
+        #print(time_start, time_end)
+
+        new_query.generate_process(time_start, time_end)
+
+        list_behavior = list()
+        list_temperature = list()
+        list_clouds = list()
+        list_precipitation = list()
+
+        for j in new_query.response_data['data']:
+
+            # Here store all values of description into weather key
+            # after call the function 'create_file_with_parameters()'
+
+            list_behavior.append(j['weather']['description'])
+            list_temperature.append(j['temp'])
+            list_precipitation.append(j['precip'])
+            list_clouds.append(j['clouds'])
+
+            # if j['weather']['description'] not in list_behavior :
+            #    list_behavior.append(j['weather']['description'])
+
+        # for l in new_query.response_data['data']:
+
+        CSV.create_file_with_parameters(
+            2019, 2019, time_start, time_end, list_behavior)
+
+        CSV.store_into_values(2019, 2019, time_start, time_end,
+                              list_temperature, list_precipitation, list_clouds)
+
+    """
     for x in days.return_days_keys():
         
         print(x)
@@ -87,11 +127,7 @@ if __name__ == "__main__":
             
             CSV.store_into_values(x,x, time_start, time_end,list_temperature, list_precipitation, list_clouds)
 
-
-
-
-
-
+    """
 
     """
     for i in range(1, len(days.get_objects()[2018])):
