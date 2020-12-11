c13033595aec98e17f1e6f97735f2816687f4052
Arturon <anegreiross@outlook.com>
2020-12-11 13:25:51 -0500

    new model testing created

--- meteorology/weatherio_API/test_model_weather_parameters.py
+++ meteorology/weatherio_API/test_model_weather_parameters.py
@@ -3,10 +3,28 @@ import pandas as pd
 import torch
 
 
+""" Libraries of the data storage """
+
+
+
 class Init_test:
-    pass
+    
+     
+
+    def __init__(self,cloud_parameter, year_consult):
+        # initialize the parameters to be the query
+        #
+
+        
+        self.path = '.csv/.clouds_parameters/.'+ cloud_parameter+'/.'+str(year_consult)+'/.'+str(year_consult)+'.csv'
+        self.dataframe_weather = pd.read_csv(self.path)
+        
+
+    def read_dataframe(self):
+
+        return self.dataframe_weather
 
 
 
-test_init = Init_test()
-
+test_init = Init_test('Broken_clouds', 2017)
+print(test_init.read_dataframe())
