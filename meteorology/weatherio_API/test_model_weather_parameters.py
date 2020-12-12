a9cc93c4975544fd8dbc8029c1a5081ce0512c29
Arturon <anegreiross@outlook.com>
2020-12-12 01:45:22 -0500

    ok

--- meteorology/weatherio_API/test_model_weather_parameters.py
+++ meteorology/weatherio_API/test_model_weather_parameters.py
@@ -1,3 +1,7 @@
+""" IN THIS FILE, WE GONNA TEST THE DATA, USING COMPARATIONS BETWEEN DATAS, AND PARAMETERS
+    
+"""
+
 import numpy as np
 import pandas as pd
 import torch
@@ -6,7 +10,14 @@ import torch
 """ Libraries of the data storage """
 
 
-from Interface_objects import make_list
+
+
+
+
+
+
+
+from Interface_objects import make_list # this one return the list o behavior to be instantiated.
 
 
 
@@ -16,8 +27,6 @@ class Init_test:
 
     def __init__(self,cloud_parameter, year_consult):
         # initialize the parameters to be the query
-        #
-
         
         self.path = '.csv/.clouds_parameters/.'+ cloud_parameter+'/.'+str(year_consult)+'/.'+str(year_consult)+'.csv'
         self.dataframe_weather = pd.read_csv(self.path)
@@ -28,11 +37,23 @@ class Init_test:
         return self.dataframe_weather
 
 
+    def compare_parameters(self,):
+        # compare two or more clouds behavior
+        # using the states, we can compare into the dataframe values
+        
 
-test_init = Init_test('Broken_clouds', 2017)
-print(test_init.read_dataframe())
+        
+
+        pass
+
+
+# Print whole behavior clouds
+# If the value is NaN is because nothing happened with that behavior
+
+for x in (make_list()):
+
+    test_init = Init_test(x, 2017)
+    print(test_init.read_dataframe())
 
 
 
-
-print(make_list())
