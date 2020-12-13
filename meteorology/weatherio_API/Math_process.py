e6dfb00ebfa6b8c2d6d5d8460689b680a2cce867
Arturon <anegreiross@outlook.com>
2020-12-12 22:08:05 -0500

    added covariance method

--- meteorology/weatherio_API/Math_process.py
+++ meteorology/weatherio_API/Math_process.py
@@ -1,15 +1,11 @@
 
-
-
 """ MATH PROCESS  """
 
 from typing import final
 
-
 class Math_process:
 
     def Generate_average_from_list(self, list_values):
-
         # This one, gonna be util, whenever we want to get average 
         # from the API values
 
@@ -17,9 +13,8 @@ class Math_process:
 
         return average
 
-    def Generate_parameters_from_regretion(self):
-        
-        # β1 and β 0
+    def Generate_parameters_from_regretion(self):        
+        # β1 and β0
 
         pass
 
@@ -37,8 +32,6 @@ class Math_process:
             clouds_sum = 0
             precip_sum = 0
 
-            
-
             for x in _object_:
 
                 if x['temperature'] is not None:
@@ -50,7 +43,6 @@ class Math_process:
                 if x['precipitation'] is not None:
                     precip_sum += int(x['precipitation'])
 
-
             final_object = {
 
                 'cloud_description': description,
@@ -58,14 +50,11 @@ class Math_process:
                 'temperature': self._define_average(temp_sum, len(_object_)),
                 'clouds': self._define_average(clouds_sum, len(_object_)),
                 'precipitation': self._define_average(precip_sum, len(_object_))
-                
                 }
-
 
             return final_object
         else:
             final_object = {
-
                 # In case that the result will not do any with the value
                 # 'None' then i will change None by "None" as String
 
@@ -73,12 +62,23 @@ class Math_process:
                 'icon':None,'code': None,
                 'temperature': None,
                 'clouds':None,
-                'precipitation': None
-                
+                'precipitation': None                
                 }
 
-
             return final_object
+
+
+    def check_covariance(self, list_values):
+        # list values is the list with whole the parameters, without the NaN parameter
+        # define a Sx and Sy as the covariance methods
+        
+        
+        
+        
+        
+        
+        pass
+
 
 
 
--- meteorology/weatherio_API/README.md
+++ meteorology/weatherio_API/README.md
@@ -1,22 +1,9 @@
 
+# Statistic model
 
+## First is set the algorithm to get all the values
 
-
-### Python para los algoritmos y la predicción y el servidor que será con Flask
-### Java para la elaboración de Android
-### Database Mongodb
-### GraphQl que es lenguaje de scripting que nos permitirá la comunicación entre el servidor y el cliente
-
-
-
-    Cliente Android envia las coordenadas de geolocalización de la persona
-
-    Servidor (Python con flask hará el mining de datos la API weatherio para descargar los datos por medio de las coordendas) ylas enviará por GraphQL al cliente
-
-    Cliente Android procesa los datos y pinta por pantalla los datos ya procesados
-
-
-
-
+    - Fetch de API page with the values.
+    - Set the libraries request from python
 
 
