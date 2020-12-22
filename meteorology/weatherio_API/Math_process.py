ecde72ffbd4ae3e301561b1bc907b5387dc5cf22
Arturon <anegreiross@outlook.com>
2020-12-22 16:55:00 -0500

    ok

--- meteorology/weatherio_API/Math_process.py
+++ meteorology/weatherio_API/Math_process.py
@@ -1,23 +1,27 @@
+#!/usr/bin/python3
+
 
 """ MATH PROCESS  """
-
-from os import truncate
-from typing import final
+import math
 from pprint import pprint
-
 
 
 class Math_process:
 
     def Generate_average_from_list(self, list_values):
-        # This one, gonna be util, whenever we want to get average 
+        # This one, gonna be util, whenever we want to get average
         # from the API values
 
         average = "{0:.3f}".format(sum(list_values) / len(list_values))
 
         return average
 
-    def Generate_parameters_from_regretion(self, objects_data):        
+    def Generate_parameters_from_regretion(self, objects_data):
+
+        #----------------------------------------------#
+        #      Generate_parameters_from_regretion      #
+        #----------------------------------------------#
+
         # β1 and β0
         # the Math model of linear regretion
         # Y = β0 + β1*x
@@ -29,7 +33,7 @@ class Math_process:
 
 
         Structure of the obejcts_data = {
-            
+
             'x': list,
             'y': list
 
@@ -43,16 +47,16 @@ class Math_process:
         MAX_X = sorted(objects_data['x'])[len(objects_data['x']) - 1]
         MIN_X = sorted(objects_data['x'])[0]
 
-        x_average = self._define_average(sum(objects_data['x']), len(objects_data['x']))
-        y_average = self._define_average(sum(objects_data['y']), len(objects_data['y']))
-
+        x_average = self._define_average(
+            sum(objects_data['x']), len(objects_data['x']))
+        y_average = self._define_average(
+            sum(objects_data['y']), len(objects_data['y']))
 
         for x in objects_data['x']:
-            list_x.append(float(x  - x_average))
+            list_x.append(float(x - x_average))
 
         for y in objects_data['y']:
             list_y.append(float(y - y_average))
-
 
         for z in objects_data['x']:
             list_pow.append(float(pow((z - x_average), 2)))
@@ -60,28 +64,26 @@ class Math_process:
         # Assuming, that both lists, has the same length
         CONST_LENGHT = len(list_x)
         total_sum = 0
-        
 
         for i in range(CONST_LENGHT):
-            total_sum += (float(list_x[i] *list_y[i]))
-
+            total_sum += (float(list_x[i] * list_y[i]))
 
         β1 = float("{0:.3f}".format((total_sum / (sum(list_pow)))))
-        β0 = float( y_average - (β1*x_average))
+        β0 = float(y_average - (β1*x_average))
 
         prediction_model = {
 
             'β1': float("{0:.3f}".format(β1)),
-            'β0':float("{0:.3f}".format(β0)),
-            'x_average':x_average,
+            'β0': float("{0:.3f}".format(β0)),
+            'x_average': x_average,
             'y_average': y_average,
-            'max_value':MAX_X,
-            'min_value':MIN_X
+            'max_value': MAX_X,  # is necessary set this kinna value
+            # in each test, the value cannot be much more of max value and lesser than min value
+            'min_value': MIN_X
 
         }
 
         return prediction_model
-
 
     # Use the math model to test the prediction
 
@@ -92,23 +94,26 @@ class Math_process:
         # assing the prediction model to variable, to access whole data stored
         object_model = self.Generate_parameters_from_regretion(objects_data)
 
-        print("[*] The prediction model is Y  = %s + X * %s  "%( object_model['β0'],object_model['β1'] ))
-        print("[*] The value to be tested in the model %s"%x_data)
+        print("[*] The prediction model is Y  = %s + X * %s  " %
+            (object_model['β0'], object_model['β1']))
+        print("[*] The value to be tested in the model %s" % x_data)
 
-        print("[*] The max %s and min %s value to be take in account"%(object_model['max_value'], object_model['min_value']))
+        print("[*] The max %s and min %s value to be take in account" %
+            (object_model['max_value'], object_model['min_value']))
 
-        
+            
+        if x_data > object_model['max_value'] or x_data < object_model['min_value']:
+            print("[x] this value cannot be used, because it is not in the stablished range.")
+        else:
+            pass
 
-
-
-    def _define_average(self,number, length):
+    def _define_average(self, number, length):
 
         average = "{0:.2f}".format(number/length)
         return float(average)
 
-
     def average(self, _object_, description):
-        # This method insert into the csv files, all the data 
+        # This method insert into the csv files, all the data
 
         if len(_object_) > 0:
 
@@ -134,12 +139,12 @@ class Math_process:
             final_object = {
 
                 'cloud_description': description,
-                'icon':_object_[0]['icon'],'relative_humidity':self._define_average(relative_humidity_sum, 
-                len(_object_)) ,'code': _object_[0]['code'],
+                'icon': _object_[0]['icon'], 'relative_humidity': self._define_average(relative_humidity_sum,
+                                                                                       len(_object_)), 'code': _object_[0]['code'],
                 'temperature': self._define_average(temp_sum, len(_object_)),
                 'clouds': self._define_average(clouds_sum, len(_object_)),
                 'precipitation': self._define_average(precip_sum, len(_object_))
-                }
+            }
 
             return final_object
         else:
@@ -148,14 +153,13 @@ class Math_process:
                 # 'None' then i will change None by "None" as String
 
                 'cloud_description': description,
-                'icon':None,'relative_humidity':None,'code': None,
+                'icon': None, 'relative_humidity': None, 'code': None,
                 'temperature': None,
-                'clouds':None,
-                'precipitation': None                
-                }
+                'clouds': None,
+                'precipitation': None
+            }
 
             return final_object
-
 
     def check_covariance(self, object_values):
         # This method will be return True o False
@@ -164,8 +168,6 @@ class Math_process:
         # set the media of values
         # use values with 3 decimals
         # asssume that the values of x and y has the same length
-        
-
         """
             Structure from the list values
             object_values = {
@@ -176,7 +178,7 @@ class Math_process:
         """
 
         # initializers
-        
+
         covariance = 1
         list_x = list()
         list_y = list()
@@ -188,7 +190,7 @@ class Math_process:
 
         average_x = float(sum(object_values['x'])/X_SIZE)
         average_y = float(sum(object_values['y'])/Y_SIZE)
-        
+
         # X
         for x in object_values['x']:
             list_x.append(float(x - average_x))
@@ -197,22 +199,18 @@ class Math_process:
         for y in object_values['y']:
             list_y.append(float(y - average_y))
 
-
-
         for i in range(X_SIZE):
-            
-            covariance += list_x[i] * list_y[i] 
 
-        Sxy = float("{0:.3f}".format(covariance /( X_SIZE - 1)))
-        
-               
+            covariance += list_x[i] * list_y[i]
+
+        Sxy = float("{0:.3f}".format(covariance / (X_SIZE - 1)))
+
         return Sxy
 
     def variance(self, object_values):
-        # As the whole values both x and y has the same lenght, 
+        # As the whole values both x and y has the same lenght,
         # then use only a constant to set the length
         # asssume that the values of x and y has the same length
-
 
         SIZE_VALUES = len(object_values['x'])
 
@@ -220,26 +218,27 @@ class Math_process:
         list_y = list()
 
         # define average from both values
-        average_x = float((sum(object_values['x']) /SIZE_VALUES))
-        average_y = float((sum(object_values['y']) /SIZE_VALUES))
-
+        average_x = float((sum(object_values['x']) / SIZE_VALUES))
+        average_y = float((sum(object_values['y']) / SIZE_VALUES))
 
         # First loops to store the difference between the value and her average
         for x in object_values['x']:
 
-            list_x.append(float(pow(float(x - average_x),2)))
+            list_x.append(float(pow(float(x - average_x), 2)))
 
         for y in object_values['y']:
 
-            list_y.append(float(pow(float(y - average_y),2)))
-        
+            list_y.append(float(pow(float(y - average_y), 2)))
+
         # Set the Variance from both values, the format is with 3 decimals
 
         # print(sum(list_x)/9)
         # print(sum(list_y) /9)
 
-        Sx = float("{0:.3f}".format(pow(float("{0:.3f}".format((sum(list_x))/(SIZE_VALUES - 1))),0.5)))
-        Sy = float("{0:.3f}".format(pow(float("{0:.3f}".format((sum(list_y))/(SIZE_VALUES - 1))),0.5)))
+        Sx = float("{0:.3f}".format(
+            pow(float("{0:.3f}".format((sum(list_x))/(SIZE_VALUES - 1))), 0.5)))
+        Sy = float("{0:.3f}".format(
+            pow(float("{0:.3f}".format((sum(list_y))/(SIZE_VALUES - 1))), 0.5)))
 
         # Return the Variance from both values
         # Doing the testing in the test_model_weather_parameters.py
@@ -277,17 +276,12 @@ class Math_process:
         Sxy = self.check_covariance(object_values)
         Sx, Sy = self.variance(object_values)
 
-        
-
-        return float("{0:.3f}".format((Sxy) / (Sx *Sy)))
-
-
+        return float("{0:.3f}".format((Sxy) / (Sx * Sy)))
 
     def set_relation_three_years(self, object_values):
         # How mucho is the difference between the actual value
         # of the parameter and the value of the average between the
         # the three years
-        
         """
             principla_object = [
                 {'2017':[list of values]},
@@ -296,35 +290,40 @@ class Math_process:
             ]
         """
 
-        average_2017 = float("{0:.3f}".format((sum(object_values[0]['2017']))/(len(object_values[0]['2017']))))
-        average_2018 = float("{0:.3f}".format((sum(object_values[1]['2018']))/(len(object_values[1]['2018']))))
-        average_2019 = float("{0:.3f}".format((sum(object_values[2]['2019']))/(len(object_values[2]['2019']))))
+        average_2017 = float("{0:.3f}".format(
+            (sum(object_values[0]['2017']))/(len(object_values[0]['2017']))))
+        average_2018 = float("{0:.3f}".format(
+            (sum(object_values[1]['2018']))/(len(object_values[1]['2018']))))
+        average_2019 = float("{0:.3f}".format(
+            (sum(object_values[2]['2019']))/(len(object_values[2]['2019']))))
 
         """print(average_2017)
         print(average_2018)
         print(average_2019)"""
-        
-        final_average = float("{0:.3f}".format((average_2017 + average_2018 + average_2019)/3))
+
+        final_average = float("{0:.3f}".format(
+            (average_2017 + average_2018 + average_2019)/3))
 
         # print(final_average)
-        print("Variation from the year 2017 is: %s percent  "%"{0:.3f}".format(((final_average - average_2017)/final_average)*100))
-        print("Variation from the year 2018 is: %s percent "%"{0:.3f}".format(((final_average - average_2018)/final_average)*100))
-        print("Variation from the year 2019 is: %s percent "%"{0:.3f}".format(((final_average - average_2019)/final_average)*100))
-        
-        
+        print("Variation from the year 2017 is: %s percent  " %
+              "{0:.3f}".format(((final_average - average_2017)/final_average)*100))
+        print("Variation from the year 2018 is: %s percent " % "{0:.3f}".format(
+            ((final_average - average_2018)/final_average)*100))
+        print("Variation from the year 2019 is: %s percent " % "{0:.3f}".format(
+            ((final_average - average_2019)/final_average)*100))
+
 
 # Testing
-
 
 math_processing = Math_process()
 
 # prediction about the sales per year
 # example extracted from the Statstics in 5th semester.
 objects_data = {
-    'x': [1.7,1.6,2.8,5.6,1.3,2.2,1.3,1.1,3.2,1.5,5.2,4.6,5.8,3],
-    'y': [3.7,3.9,6.7,9.5,3.4,5.6,3.7,2.7,5.5,2.9,10.7,7.6,11.8,4.1]
+    'x': [1.7, 1.6, 2.8, 5.6, 1.3, 2.2, 1.3, 1.1,3.2,1.5,5.2,4.6,5.8,3],
+    'y': [3.7, 3.9, 6.7, 9.5, 3.4, 5.6, 3.7, 2.7,5.5,2.9,10.7,7.6,11.8,4.1]
 }
 
 
 pprint(math_processing.Generate_parameters_from_regretion(objects_data))
-math_processing.test_math_model(objects_data,5)
+math_processing.test_math_model(objects_data, 5)
