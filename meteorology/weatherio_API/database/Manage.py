80315dfdc93bd88c1a05926732cbd45f4972c7ee
Arturo Negreiros <anegreiross@outlook.com>
2021-02-14 01:20:35 -0500

    ok

--- meteorology/weatherio_API/database/Manage.py
+++ meteorology/weatherio_API/database/Manage.py
@@ -1,20 +1,36 @@
 import mysql.connector
 
 
-
-
-class Connection:
+"""class Connection:
 
     def __init__(self):
-        
-        self.connector = None
 
+        self.connector = None
 
     def generate_connection(self):
 
-        self.connector = mysql.connector.connect(
-            host= "localhost",
-            user= "root_arturo",
-            password= "LikeAStone",
-            database= "Meteorology"
+        connector = mysql.connector.connect(
+            host="localhost",
+            user="root_arturo",
+            password="LikeAStone",
+            database="MeteorologyPredictions"
         )
+
+        return connector.cursor()
+
+"""
+connector = mysql.connector.connect(
+    host="localhost",
+    user="root_arturo",
+    password="LikeAStone",
+    database="MeteorologyPredictions"
+)
+
+# print(Connection().connector)()
+cursor = connector.cursor()
+cursor.execute("SHOW TABLES")
+result = cursor.fetchall()
+
+print(result)
+for x in result:
+    print(x)
